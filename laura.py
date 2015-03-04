8dbafcd60b8d9425ee55d61b640359e8398aee7e
Toph Allen <toph.allen@gmail.com>
2015-03-04 15:40:11 -0500

    Committing uncommitted changes
    I should probably have committed these a while ago.

--- laura.py
+++ laura.py
@@ -1,7 +1,8 @@
 import os
 import csv
-from time import sleep
+import time
 import random
+import string
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -32,10 +33,11 @@ class pub_data:
         getter.go_to_full_article()
         getter.assign_article_current_page_source(self)
 
-    def write_html_to_file(self):
+    def write_html_to_file(self, outdir):
+        valid_chars = "-_.() {}{}".format(string.ascii_letters, string.digits)
         filename = (str(self.number) + ' ' + self.title.lower()).replace(' ', '_')[0:64] + '.html'
-        outdir = os.getcwd() + '/laura_out/'
-        file = open(outdir + filename, 'w')
+        filename = ''.join(c for c in filename if c in valid_chars)
+        file = open(os.path.join(outdir, filename), 'w')
         file.write(self.html)
         file.close()
 
@@ -48,10 +50,16 @@ class pub_html_getter(webdriver.Firefox):
 
     def search_scholar_for_article(self, article):
         self.get('http://scholar.google.com')
-        self.find_element_by_id('gs_hp_tsi').send_keys(article.search_string, Keys.RETURN)
+        try:
+            self.find_element_by_id('gs_hp_tsi').send_keys(article.search_string, Keys.RETURN)
+        except:
+            pass
 
     def click_first_result(self):
-        self.find_element_by_class_name("gs_rt").find_element_by_tag_name('a').click()
+        try:
+            self.find_element_by_class_name("gs_rt").find_element_by_tag_name('a').click()
+        except:
+            pass
 
     def ezproxy_login(self):
         ezproxy_url = 'http://ezproxy.cul.columbia.edu/login?url='
@@ -60,45 +68,44 @@ class pub_html_getter(webdriver.Firefox):
         try:
             self.find_element_by_name('username').send_keys(self.ezproxy_username)
             self.find_element_by_name('password').send_keys(self.ezproxy_password, Keys.RETURN)
-        except NoSuchElementException:
+        except:
             pass
 
     def go_to_full_article(self):
         try:
             return self.find_element_by_partial_link_text('HTML').click()
-        except NoSuchElementException:
+        except:
             pass
         try:
             return self.find_element_by_partial_link_text('Full Article').click()
-        except NoSuchElementException:
+        except:
             pass
         try:
             return self.find_element_by_partial_link_text('Full').click()
-        except NoSuchElementException:
+        except:
             pass
         try:
             return self.find_element_by_partial_link_text('Article').click()
-        except NoSuchElementException:
+        except:
             pass
-        raise NoSuchElementException
 
     def article_link_search_test(self):
         try:
             return self.find_element_by_partial_link_text('HTML').text
-        except NoSuchElementException:
+        except:
             pass
         try:
             return self.find_element_by_partial_link_text('Full Article').text
-        except NoSuchElementException:
+        except:
             pass
         try:
             return self.find_element_by_partial_link_text('Full').text
-        except NoSuchElementException:
+        except:
             pass
         raise NoSuchElementException
         try:
             return self.find_element_by_partial_link_text('Article').text
-        except NoSuchElementException:
+        except:
             pass
 
     def assign_article_current_page_source(self, article):
@@ -106,21 +113,40 @@ class pub_html_getter(webdriver.Firefox):
 
 
 def main():
+    start_time = time.strftime("%Y-%m-%d_%H-%M-%S")
+    outdir = os.getcwd() + '/laura_out/' + start_time
+    if not (os.path.exists(outdir)):
+        os.makedirs(outdir)
+    outcsv = open(os.path.join(outdir, 'laura_pub_urls.csv'), 'a')
+    outcsv.write(',Number,Journal,Vol,Year,Issue,First.pg,Title,URL\n')
+    outcsv.close()
+
+
     pub_dataframe = pd.io.parsers.read_table('data/laura_pubs_meta.csv', encoding='utf-8', quotechar='"', sep=',', index_col=0)
     pub_dataframe['URL'] = ''
 
-    pub_dataframe = pub_dataframe.iloc[0:10, :]
+    num_pubs = len(pub_dataframe)
+
+    # Optionally operate on only a part of the list.
+    # Percentage complete shows how far you are through the whole list.
+    pub_dataframe = pub_dataframe.iloc[1090:, :]
 
     getter = pub_html_getter()
 
     for i, row in pub_dataframe.iterrows():
+        print('Downloading article {} of {} '
+              '({:.2%} complete).'.format(i, num_pubs, i / num_pubs))
         article = pub_data(row)
         article.get_html_for_article(getter)
-        article.write_html_to_file()
-        pub_dataframe.loc[i, "URL"] = getter.current_url
-        sleep(random.lognormvariate(1.5,0.5))
+        article.write_html_to_file(outdir)
+        row.loc['URL'] = getter.current_url
+        row = DataFrame(row).T
+        with open(os.path.join(outdir, 'laura_pub_urls.csv'), 'a') as outcsv:
+            row.to_csv(outcsv, header=False)
+        time.sleep(random.lognormvariate(0.75,0.5))
 
-    pub_dataframe.to_csv(os.getcwd() + '/laura_out/laura_pub_urls.csv')
+    # outcsv.close()
+    getter.close()
 
 
 if __name__ == '__main__':
